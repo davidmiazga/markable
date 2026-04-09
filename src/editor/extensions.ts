a239105e71ff32927ff372d1308fadfa7a93f2f0
David Miazga <davidmiazga@yahoo.com>
2026-04-08 20:42:23 -0500

    Links styled

--- src/editor/extensions.ts
+++ src/editor/extensions.ts
@@ -7,6 +7,8 @@
 
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
+import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
+import { tags } from "@lezer/highlight";
 import { HighlightExtension } from "./highlight-ext";
 import { Compartment, type Extension, Prec } from "@codemirror/state";
 import { EditorView, keymap } from "@codemirror/view";
@@ -50,6 +52,26 @@ export const previewTheme = EditorView.theme({
   },
 });
 
+/**
+ * Override defaultHighlightStyle colors so our CSS-variable-based
+ * theme controls all syntax colors. Headings inherit from the
+ * line-decoration classes (.cm-live-h1 etc.) applied by live-preview.
+ */
+const themeHighlight = HighlightStyle.define([
+  { tag: tags.link, color: "var(--link-color)", textDecoration: "underline" },
+  { tag: tags.url, color: "var(--link-color)" },
+  { tag: tags.heading1, color: "inherit", fontWeight: "inherit" },
+  { tag: tags.heading2, color: "inherit", fontWeight: "inherit" },
+  { tag: tags.heading3, color: "inherit", fontWeight: "inherit" },
+  { tag: tags.heading4, color: "inherit", fontWeight: "inherit" },
+  { tag: tags.heading5, color: "inherit", fontWeight: "inherit" },
+  { tag: tags.heading6, color: "inherit", fontWeight: "inherit" },
+  { tag: tags.strong, color: "inherit" },
+  { tag: tags.emphasis, color: "inherit" },
+  { tag: tags.strikethrough, color: "inherit" },
+  { tag: tags.quote, color: "var(--blockquote-color)", fontStyle: "italic" },
+]);
+
 /** All preview-mode extensions bundled together. */
 export const previewExtensions: Extension = [livePreviewExtension, previewTheme];
 
@@ -72,6 +94,7 @@ export function buildExtensions(): Extension[] {
   extensions.push(EditorView.lineWrapping);
   extensions.push(Prec.high(keymap.of(formatKeymap)));
   extensions.push(baseTheme);
+  extensions.push(syntaxHighlighting(themeHighlight));
   extensions.push(previewCompartment.of(previewExtensions));
 
   return extensions;
